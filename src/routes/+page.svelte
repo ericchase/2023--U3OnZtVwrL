689b0bb81340e70331a81ffc15e79dd62cdfdf60
ericchase <github@eric.addymail.com>
2023-09-15 15:42:30 -0500

    build page
    Signed-off-by: ericchase <github@eric.addymail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Component from '$lib/component.svelte';
+</script>
+
+<main>
+  <Component />
+</main>
+
+<style>
+  main {
+    margin: 1em;
+  }
+</style>
