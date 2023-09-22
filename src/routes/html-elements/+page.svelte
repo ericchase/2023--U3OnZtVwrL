bbb26b1ed17a3e57277d55d58d46a3af9dae1f3d
ericchase <github@eric.addymail.com>
2023-09-22 14:29:14 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- src/routes/html-elements/+page.svelte
+++ src/routes/html-elements/+page.svelte
@@ -11,13 +11,13 @@
     </div>
   </div>
   <div class="col">
-    <a href="https://necolas.github.io/normalize.css/">normalize.css</a>
+    <a class="link" href="https://necolas.github.io/normalize.css/">normalize.css</a>
     <div class="wrapper normalize">
       <ElementsList />
     </div>
   </div>
   <div class="col">
-    <a href="https://andy-bell.co.uk/a-more-modern-css-reset/">Modern CSS Reset</a>
+    <a class="link" href="https://andy-bell.co.uk/a-more-modern-css-reset/">Modern CSS Reset</a>
     <div class="wrapper reset">
       <ElementsList />
     </div>
